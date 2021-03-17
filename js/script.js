daff6134f5a952c93468e09b86d0cd4942f129bb
damoon darvishi <damoon.darvishi@gmail.com>
2021-03-17 19:59:31 +0330

    forecast

--- js/script.js
+++ js/script.js
@@ -7,12 +7,47 @@ let wind = document.querySelector('.weather__indicator--wind>.value');
 let pressure = document.querySelector('.weather__indicator--pressure>.value');
 let image = document.querySelector('.weather__image');
 let temperature = document.querySelector('.weather__temperature>.value');
-// let firecastBlock = document.querySelector('.weather__forecast');
+let forecastBlock = document.querySelector('.weather__forecast');
+
+let weatherImages = [
+    {
+        url: 'images/clear-sky.png',
+        ids: [800]
+    },
+    {
+        url: 'images/broken-clouds.png',
+        ids: [803, 804]
+    },
+    {
+        url: 'images/few-clouds.png',
+        ids: [801]
+    },
+    {
+        url: 'images/mist.png',
+        ids: [701, 711, 721, 731, 741, 751, 761, 771, 781]
+    },
+    {
+        url: 'images/scattered-clouds.png',
+        ids: [802]
+    },
+    {
+        url: 'images/shower-rain.png',
+        ids: [520, 521, 522, 531, 300, 301, 302, 310, 311, 312, 313, 314, 321]
+    },
+    {
+        url: 'images/snow.png',
+        ids: [511, 600, 601, 602, 611, 612, 612, 613, 615, 616, 620, 621, 622]
+    },
+    {
+        url: 'images/thunderstorm.png',
+        ids: [200, 201, 202, 210, 211, 212, 221, 230, 231, 232]
+    },
+]
 
 // configure
 let weatherAPIKey = '6d075ff22293bedd6395d2de89610d97';
 let weatherBaseEndpoint = 'https://api.openweathermap.org/data/2.5/weather?units=metric&appid=' + weatherAPIKey;
-// let forecastBaseEndpoint = 'https://api.openweathermap.org/data/2.5/forecast?units=metric&appid=' + weatherAPIKey;
+let forecastBaseEndpoint = 'https://api.openweathermap.org/data/2.5/forecast?units=metric&appid=' + weatherAPIKey;
 
 let getWeatherByCityName = async (city) => {
     let endpoint = weatherBaseEndpoint + '&q=' + city;
@@ -21,30 +56,31 @@ let getWeatherByCityName = async (city) => {
     return weather;
 }
 
-// let getForecastByCityID = async (id) => {
-//     let endpoint = forecastBaseEndpoint + '&id=' + id;
-//     let result = await fetch(endpoint);
-//     let forecast = await result.json();
-//     let forecastList = forecast.list;
-//     let daily = [];
-//     console.log(forecast)
+let getForecastByCityID = async (id) => {
+    let endpoint = forecastBaseEndpoint + '&id=' + id;
+    let result = await fetch(endpoint);
+    let forecast = await result.json();
+    let forecastList = forecast.list;
+    let daily = [];
+    console.log(forecast)
 
-//     forecastList.forEach(day => {
-//         let date = new Date(day.dt_txt.replace(' ', 'T'));
-//         let hours = date.getHours();
-//         if(hours == 12) {
-//             daily.push(day)
-//         }
-//     })
-//     console.log(daily)
-// }
+    forecastList.forEach(day => {
+        let date = new Date(day.dt_txt.replace(' ', 'T'));
+        let hours = date.getHours();
+        if(hours == 12) {
+            daily.push(day)
+        }
+    })
+    return daily;
+}
 
 searchInp.addEventListener('keydown', async (e) => {
     if(e.keyCode === 13) {
         let weather = await getWeatherByCityName(searchInp.value)
-        // let cityID = weather.id;
+        let cityID = weather.id;
         updateCurrentWeather(weather);
-        // getForecastByCityID(cityID)
+        let forecast = await getForecastByCityID(cityID);
+        updateForecast(forecast)
     }
 })
 
@@ -66,20 +102,36 @@ let updateCurrentWeather = (data) => {
         windDirection = 'North';
     }
     wind.textContent = windDirection + ', ' + data.wind.speed;
-    temperature.textContent = data.main.temp > 0 ?
-                                "+" + (data.main.temp) : (data.main.temp)
+    temperature.textContent = data.main.temp > 0 ? "+" + (data.main.temp) : (data.main.temp);
+
+    let imgID = data.weather[0].id;
+    weatherImages.forEach(obj => {
+        if(obj.ids.includes(imgID)) {
+            image.src = obj.url;
+        }
+    })
 }
 
 
-// forecast
-// let updateForecast = (forecast) => {
-//     forecastBlock.innerHTML = '';
-//     forecast.forEach(day => {
-//         let iconUrl = 'https://openweathermap.org/img/wn/' + day.weather[0].icon + '2x.png';
-//     })
-// }
+let updateForecast = (forecast) => {
+    forecastBlock.innerHTML = '';
+    forecast.forEach(day => {
+        let iconUrl = 'https://openweathermap.org/img/wn/' + day.weather[0].icon + '@2x.png';
+        let dayName = dayOfWeek(day.dt * 1000);
+        console.log(dayName)
+        let temperature = day.main.temp > 0 ? "+" + (day.main.temp) : (day.main.temp);
+        let forecastItem = `
+            <article class="weather__forecast__item"> 
+                <img src="${iconUrl}" alt="${day.weather[0].description}" class="weather__forecast__icon">
+                <h3 class="weather__forecast__day">${dayName}</h3>
+                <p class="weather__forecast__temperature"><span class="value">${temperature}</span>&deg;C</p>
+            </article>
+        `;
+        forecastBlock.insertAdjacentHTML('beforeend', forecastItem)
+    })
+}
 
 
-let dayOfWeek = () => {
+let dayOfWeek = (dt = new Date().getTime()) => {
     return new Date().toLocaleDateString('en-EN', {'weekday': 'long'})
 }
